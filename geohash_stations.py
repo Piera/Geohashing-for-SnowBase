f88db10947a2657d2e3683129f8b52223077ec4f
Piera <pierad@gmail.com>
2015-03-17 15:54:46 -0700

    Corrected density

--- geohash_stations.py
+++ geohash_stations.py
@@ -71,8 +71,9 @@ def lookup():
 			all()
 		neighborhoods = neighborhoods + neighbor
 	dist_list = []
+
 	# For all of the stations found in neighborhoods, check for data and snow. 
-	# If there is data and snow for a given station, add it to the list
+	# If there is data and snow for a given station, add it to the heap
 	for location in neighborhoods:
 		try: 
 			station = dbsession.query(model.Station).filter(model.Station.id == location.station_id).one()
@@ -82,26 +83,26 @@ def lookup():
 			kms = int(distance(origin, destination))
 			mi = int(0.621371*kms)
 			if snow.depth != None and snow.depth > 0:
-				if snow.water_equiv != None and snow.water_equiv != 0:
-					density = (int((snow.water_equiv / snow.depth) * 100))
-					if density > 100:
-							density = 100
-				else: 
-					density = "No Data" 
 				heapq.heappush(dist_list, (mi, station.id))
 			else:
 				continue
 		except IndexError:
 			continue
+	
 	# Return the 10 closest stations, their distances away in miles (converted from kms)
 	#  and basic telemetry data for that station
-
 	closest_sta = [heapq.heappop(dist_list) for i in range(10)]
 	responses_list = []
 	for station in closest_sta:
-		print station[0], station[1]
 		mi = station[0]
 		station = dbsession.query(model.Station).filter(model.Station.id == station[1]).one()
+		snow = station.snow_data[-1]
+		if snow.water_equiv != None and snow.water_equiv != 0:
+			density = (int((snow.water_equiv / snow.depth) * 100))
+			if density > 100:
+				density = 100
+			if density == None:
+				density = "No Data" 
 		responses_list.append({'dist':mi, 'text-code':station.id, 'id':station.given_id, 'ele':station.elevation,\
 				'lat':station.latitude, 'lng':station.longitude, 'name':station.name, 'depth':snow.depth,\
 				'depth_change':snow.depth_change, 'density':density, 'date':snow.date.strftime("%m/%d/%y %H:%M")})
